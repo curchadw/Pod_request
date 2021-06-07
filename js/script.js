22a61343c27672804189af224ad8fc16fda0b801
curtis <laurencechadwell@gmail.com>
2021-06-06 19:30:01 -0500

    updated api calls

--- js/script.js
+++ js/script.js
@@ -1,20 +1,77 @@
-let bearer_token = 'BQDcF0pMD6p-41Y0PyN1RJWXjqaX4WEFfzOI4PZNZLSjhh0HKkcMHfcmJ7iaCjm2tqCModIy2fqOJi9E6CpFmCIhYOzsaLk7udRH7ViN2lY5BqTZo-uvJ8dX9PQot-VvuImF6BHJ7IlVjknKdifu04gIHXMeuEPg2InXlglX0-hdZ242toScJEisdFvSVwJMXowiEEKwFUqRmAjY5JZuB8c4z77hsKjyTJAZe1Aifgq8M6R_dkwtiZH5DPnUW7P-zC7qOKzTv1B1AG2l6GfG275b8BmzsDWpCFRNsxCI'
-let url = `https://api.spotify.com/v1/shows`
-
-let bearer = 'Bearer ' + bearer_token
-
-
-const fetch_featured = ()=>{
+const bearer_token = 'BQAi1__fKkR-A3uncY4JLHzRMlw7PkBkvbCAXAe9twvbZV1PGUk6AXrlnfclO_Zwty146pIy-dluBkGAhb3cZ3RzceaJ8n-1xQtZvlWp6hyYSjGgJvzmuWDobHfvJgevTx617X3-vjFU-BRVkto_z5vKW1AmVyBZhjvafBti4dxQSbmfDYof_2etg3ZT52vi-qfKTyoZL_I7Q_RWp8TvuTUlBv9R8oFI65GgMVYp-mYt8pz6azQSFshJOZRqboK8VNobCIHITQ8sVL9BiJx0hV82QUnHLqG2T4-NoWcp'
+const url = `https://api.spotify.com/v1/shows`
+const bearer = 'Bearer ' + bearer_token
+const header = {
+    method:"GET",
+    headers:{
+    'Authorization': bearer,
+    'Accept':'application/json',
+    'Content-Type':'application/json',
+    }
+}
+//data.shows[0].explicit
+function fetch_featured(){
+    
     let show_id = "2nIvarXvvZcp1cePx69x9N"
     fetch(url+"?ids="+show_id+"&market=US",{
         method:"GET",
-        header:{
-            'Authorization':bearer,
+        headers:{
+            'Authorization': bearer,
             'Accept':'application/json',
-            'Content-Type':'application/json'
+            'Content-Type':'application/json',
         }
     })
     .then((data) => {return data.json()})
-    .then((data)=>{console.log(data)})
+    .then((data)=>{console.log(data)
+        let res = `<div class='show_container'>
+        <a href='${data.shows[0].uri}'><img src='${data.shows[0].images[1].url}'/></a>
+        <div class='description'>
+            <h2>${data.shows[0].name}</h2>
+            <p>${data.shows[0].description}</p>
+            <h3>${data.shows[0].explicit ? "Warning, content may contain language not suitable for childern." : 0}</h3>
+            
+        </div>
+        </div>`
+
+        document.getElementById('feature').innerHTML = res
+    })
     .catch(error => {console.error(error)})
+}
+
+
+const first_id = '6xpiit8aJmwDHk1rKdxmri'
+const second_id ='08Ft1wtrsnYYHrkTdTQM5s'
+const third_id = '3V8TSvhtTRohclApkh2xIu'
+
+
+const multi_fetch = () =>{
+    Promise.all([
+        fetch(url + '/' + `${first_id}`,header),
+        fetch(url + '/' + `${second_id}`,header),
+        fetch(url + '/' + `${third_id}`,header)
+    ])
+    .then(data =>{
+        return Promise.all(data.map(data =>{ return data.json()}))
+    })
+    .then(data => {
+    console.log(data)
+
+    data.forEach(show =>{
+            let show_html = `<div class='latest_show'>
+            <div class='inner_latest_show'>
+                <a href='${show.uri}'><img src='${show.images[1].url}'/></a>
+                <h3>${show.name}</h3>
+            </div>
+            </div>`
+
+            document.getElementById('latest_show').innerHTML += show_html
+        })
+    })
+    .catch(error => {console.error(error)})
+}
+
+
+const fetch_all = () => {
+    fetch_featured()
+    multi_fetch()
 }
